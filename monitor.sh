1bde2ae126c4e9971e66960ba320567073387741
rewse <36141290+rewseRE@users.noreply.github.com>
2021-08-29 20:09:58 +0300

    init

--- monitor.sh
+++ monitor.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Node name, e.g. "Cosmos"
-NODE_NAME=""
+NODE_NAME="Sentinel"
 # File name for saving parameters, e.g. "cosmos.log"
-LOG_FILE=""
+LOG_FILE="sentinel.log"
 # Your node RPC address, e.g. "http://127.0.0.1:26657"
 NODE_RPC=""
 # Trusted node RPC address, e.g. "https://rpc.cosmos.network:26657"
-SIDE_RPC=""
+SIDE_RPC="https://rpc.sentinel.co"
 # Telegram bot API
 TG_BOT=""
 # Telegram chat ID
